6cb25fd43cfc4d14a154e3b278c5e53b24f2a853
Rian Lucas <rianlucas2014@gmail.com>
2024-04-18 09:21:42 -0300

    feat: using locks

--- src/ParallelFileReader.java
+++ src/ParallelFileReader.java
@@ -3,18 +3,22 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 
 public class ParallelFileReader {
-    private static final int BUFFER_SIZE = 1024; // Adjust as needed
-    private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors(); // Number of available processors
-    private static final Lock channelLock = new ReentrantLock(); // Lock for synchronizing access to FileChannel
+    private static final int BUFFER_SIZE = 50000000; // Adjust as needed
+    private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors() - 1; // Number of available processors
+    private static final Lock channelLock = new ReentrantLock();
 
+    private static final HashMap<String, List<String>> transactionsPerCountryResult = new HashMap<>();
+    // Lock for synchronizing access to FileChannel
     public static void main(String[] args) {
-        File file = new File("large.csv");
+        File file = new File("large_dataset_5M.csv");
 
         // Get file size
         long fileSize = file.length();
@@ -39,7 +43,7 @@ public class ParallelFileReader {
                 end = findNearestEOL(channel, end);
 
                 // Create a new thread for processing
-                Thread thread = new Thread(new CSVReaderThread(start, end, channel));
+                Thread thread = new Thread(new CSVReaderThread(start, end, channel, i + 1));
                 threads.add(thread);
                 thread.start();
 
@@ -50,6 +54,10 @@ public class ParallelFileReader {
             // Wait for all threads to finish
             for (Thread thread : threads) {
                 thread.join();
+            }
+
+            for (Map.Entry<String, List<String>> entry : transactionsPerCountryResult.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue().size());
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -119,18 +127,28 @@ public class ParallelFileReader {
         private long end;
         private FileChannel channel;
 
-        public CSVReaderThread(long start, long end, FileChannel channel) {
+        private int i;
+
+        HashMap<String, List<String>> transactionsPerCountry = new HashMap<>();
+
+
+        public CSVReaderThread(long start, long end, FileChannel channel, int i) {
             this.start = start;
             this.end = end;
             this.channel = channel;
+            this.i = i;
         }
 
         @Override
         public void run() {
             try {
+                long startTime = System.currentTimeMillis();
+                System.out.printf("thread %d started %d%n", i, startTime);
+                System.out.println();
                 // Create a ByteBuffer for reading data
                 StringBuilder lineBuilder = new StringBuilder();
-
+                String csvDivisor = ",";
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 // Move to the start position
                 long bytesRead = 0;
                 long totalBytes = end - start;
@@ -156,7 +174,26 @@ public class ParallelFileReader {
                             // End of line reached, parse the line
                             String line = lineBuilder.toString();
                             // Process the CSV line (you can implement your CSV parsing logic here)
-                            System.out.println("Parsed CSV line: " + line);
+                            String[] transaction = line.split(csvDivisor);
+
+                            String transactionId = transaction[0];
+                            String userId = transaction[1];
+                            String companyId = transaction[2];
+                            Date transactionDate = formatter.parse(transaction[3]);
+                            String productId = transaction[4];
+                            String productDescription = transaction[5];
+                            int quantity = Integer.parseInt(transaction[6]);
+                            Double pricePerUnit = Double.parseDouble(transaction[7]);
+                            String currency = transaction[8];
+                            String paymentMethod = transaction[9];
+                            String country = transaction[10];
+                            String city = transaction[11];
+
+                            if(transactionsPerCountry.containsKey(country)){
+                                transactionsPerCountry.get(country).add(transactionId);
+                            } else {
+                                transactionsPerCountry.put(country, new ArrayList<>(List.of(transactionId)));
+                            }
                             // Reset the StringBuilder for the next line
                             lineBuilder.setLength(0);
                         } else {
@@ -165,8 +202,24 @@ public class ParallelFileReader {
                     }
                     buffer.clear();
                 }
+
+                channelLock.lock();
+                for (Map.Entry<String, List<String>> entry : transactionsPerCountry.entrySet()) {
+                    if(transactionsPerCountryResult.containsKey(entry.getKey())){
+                        transactionsPerCountryResult.get(entry.getKey()).addAll(entry.getValue());
+                    } else {
+                        transactionsPerCountryResult.put(entry.getKey(), (entry.getValue()));
+                    }
+                }
+                channelLock.unlock();
+                long endTime = System.currentTimeMillis();
+                long executionTime = endTime - startTime;
+                System.out.println("Thread: " + i + " execution time: " + executionTime + "ms");
+
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
             }
         }
     }
